faee309d2a3fa7ec264d0d6ae2d9f9540c1637d6
panvla <panvla@gmail.com>
2022-06-11 01:56:10 +0200

    added initial angular project

---
+++ frontend/angular-ecommerce/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProductListComponent } from './components/product-list/product-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
