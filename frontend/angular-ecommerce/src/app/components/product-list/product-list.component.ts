d12a41dfcb74ff4ca31636f005c9def0befcbc96
panvla <panvla@gmail.com>
2022-06-18 19:53:26 +0200

    added cart-status component.closes #16

--- frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -101,4 +101,11 @@ export class ProductListComponent implements OnInit {
   }
 
 
+  addToCart(theProduct: Product){
+    console.log( `Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
+
+
+  }
+
+
 }
