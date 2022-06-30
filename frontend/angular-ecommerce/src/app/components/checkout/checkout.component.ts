00f1aafbe5b409d5e4d565a677e4246c3c3aebd6
panvla <pandvlado@gmail.com>
2022-06-30 21:23:10 +0200

    added customer angular validators

--- frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Country } from 'src/app/common/country';
+import { State } from 'src/app/common/state';
 import { Love2ShopFormService } from 'src/app/services/love2-shop-form.service';
 
 @Component({
@@ -17,15 +19,21 @@ export class CheckoutComponent implements OnInit {
   creditCardYears: number[] = [];
   creditCardMonths: number[] = [];
 
+  countries: Country[] = [];
+
+  shippingAddressStates: State[] = [];
+
+  billingAddressStates: State[] = [];
+
   constructor(private formBuilder: FormBuilder, private love2ShopFormService: Love2ShopFormService) { }
 
   ngOnInit(): void {
 
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: ['']
+        firstName: new FormControl('',[Validators.required, Validators.minLength(2)]),
+        lastName: new FormControl('',[Validators.required, Validators.minLength(2)] ),
+        email: new FormControl('', [Validators.required, Validators.pattern('')])
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
@@ -68,11 +76,23 @@ export class CheckoutComponent implements OnInit {
       }
     );
 
+    //populate countries
+
+    this.love2ShopFormService.getCountries().subscribe(
+      data => {
+        console.log("Retrived countries: " + JSON.stringify(data));
+        this.countries = data;
+      }
+    )
+
   }
 
   onSubmit(){
     console.log("Handling the submit button");
     console.log("The email address is " + this.checkoutFormGroup.get('customer').value.email);
+
+    console.log("The shipping address country is " + this.checkoutFormGroup.get('shippingAddress').value.country.name);
+    console.log("The shipping address state is " + this.checkoutFormGroup.get('shippingAddress').value.state.name);
   }
 
   copyShippingAddressToBillingAddress(event){
@@ -80,9 +100,15 @@ export class CheckoutComponent implements OnInit {
     if(event.target.checked){
       this.checkoutFormGroup.controls['billingAddress']
         .setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
+
+        //bug fix for states
+        this.billingAddressStates = this.shippingAddressStates;
     }
     else{
       this.checkoutFormGroup.controls['billingAddress'].reset();
+
+      //bug fix for states
+      this.billingAddressStates = [];
     }
   }
 
@@ -106,6 +132,33 @@ export class CheckoutComponent implements OnInit {
       data => {
         console.log("Retrieved credit card months: " + JSON.stringify(data));
         this.creditCardMonths = data;
+      }
+    )
+
+  }
+
+  getStates(formGroupName: string){
+
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+
+    const countryCode = formGroup.value.country.code;
+    const countryName = formGroup.value.country.name;
+
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country name: ${countryName}`);
+
+    this.love2ShopFormService.getStates(countryCode).subscribe(
+      data => {
+        if(formGroupName === 'shippingAddress'){
+          console.log("Retrived states: " + JSON.stringify(data));
+          this.shippingAddressStates = data;
+
+        }
+        else{
+          this.billingAddressStates = data;
+        }
+
+        formGroup.get('state').setValue(data[0]);
       }
     )
 
